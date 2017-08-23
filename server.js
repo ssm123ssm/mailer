fa6896e40c9e25e1cd941672c5337e00bf371dd8
ssm123ssm <ssm123ssm@gmail.com>
2017-08-23 09:58:18 +0530

    test2

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.post('/signup', function (req, res) {
                                                 if (error) {
                                                     return console.log(error);
                                                 }
-                                                console.log('Message %s sent: %s', info.messageId, info.response);
+                                                console.log('\n\nMessage sent to User');
                                             });
 
 
@@ -194,7 +194,7 @@ app.post('/signup', function (req, res) {
                                                             return console.log(error);
                                                         }
                                                         console.log("\n\nMAIL SENT!");
-                                                        console.log('Message %s sent: %s', info.messageId, info.response);
+                                                        console.log('\n\nMessage sent to YOU');
                                                     });
                                                 }
                                             });
