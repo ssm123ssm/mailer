62c4e233e9efb27c00385d10df943cd772acf5ff
ssm123ssm <ssm123ssm@gmail.com>
2017-08-23 11:05:56 +0530

    email fix

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ app.post('/signup', function (req, res) {
                                                     var allDocs = all.length;
                                                     var mailOptions2 = {
                                                         from: 'mailer', // sender address
-                                                        to: "mailer.ssm.app@gmail.com", // list of receivers
+                                                        to: "ssm123ssm@gmail.com", // list of receivers
                                                         subject: "New Registraion on Mailer!", // Subject line
                                                         text: "New Registraion on Mailer!", // plain text body
                                                         html: mail_template.reg + `Time: ${Date()} <br> Email: ${email} <br>Username: ${username}<br><br>Currently Registered Users: ${allDocs}` // html body
